7e370fc721c868bab22daf3b770163e94b05b8ef
Joe Riley <joeyriley@gmail.com>
2021-01-22 16:21:26 -0700

    Now get stuck on validating the password

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,28 @@
 // Initialize navbar
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   const elems = document.querySelectorAll('.sidenav');
   let options = [];
   const instances = M.Sidenav.init(elems, options);
 });
 
 // modal trigger
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   var elems = document.querySelectorAll('.modal');
   let options = [];
   var instances = M.Modal.init(elems, options);
 });
+
+// confirm password on register after entering confirm password
+const passwordEl = document.getElementById('password');
+const confirmPasswordEl = document.getElementById('confirm_password');
+console.log(`password1: ${passwordEl.textContent}, password2: ${confirmPasswordEl.textContent}`)
+confirmPasswordEl.onkeyup = () => {
+  console.log('hi');
+  if(passwordEl.textContent !== confirmPasswordEl.textContent) {
+    confirmPasswordEl.setCustomValidity('Passwords Do Not Match')
+  } else {
+    confirmPasswordEl.setCustomValidity('');
+  }
+
+};
+
