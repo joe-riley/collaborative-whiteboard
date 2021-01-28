75e876321d0136657c6094ec080c1350c71d1d67
Joe Riley <rileyjoseph@gmail.com>
2021-01-27 23:21:53 -0700

    Fix bug where drawing elements are not rendered yet

--- public/js/drawing.js
+++ public/js/drawing.js
@@ -13,6 +13,7 @@
   fabric.Object.prototype.transparentCorners = false;
 
   var drawingModeEl = $("drawing-mode"),
+    drawingModeSelector = $("drawing-mode-selector"),
     drawingOptionsEl = $("drawing-mode-options"),
     drawingColorEl = $("drawing-color"),
     drawingShadowColorEl = $("drawing-shadow-color"),
@@ -21,20 +22,24 @@
     drawingShadowOffset = $("drawing-shadow-offset"),
     clearEl = $("clear-canvas");
 
-  clearEl.onclick = function () {
-    canvas.clear();
-  };
+  if (clearEl) {
+    clearEl.onclick = function () {
+      canvas.clear();
+    };
+  }
 
-  drawingModeEl.onclick = function () {
-    canvas.isDrawingMode = !canvas.isDrawingMode;
-    if (canvas.isDrawingMode) {
-      drawingModeEl.innerHTML = "Cancel drawing mode";
-      drawingOptionsEl.style.display = "";
-    } else {
-      drawingModeEl.innerHTML = "Enter drawing mode";
-      drawingOptionsEl.style.display = "none";
-    }
-  };
+  if (drawingModeEl) {
+    drawingModeEl.onclick = function () {
+      canvas.isDrawingMode = !canvas.isDrawingMode;
+      if (canvas.isDrawingMode) {
+        drawingModeEl.innerHTML = "Cancel drawing mode";
+        drawingOptionsEl.style.display = "";
+      } else {
+        drawingModeEl.innerHTML = "Enter drawing mode";
+        drawingOptionsEl.style.display = "none";
+      }
+    };
+  }
 
   if (fabric.PatternBrush) {
     var vLinePatternBrush = new fabric.PatternBrush(canvas);
@@ -116,61 +121,77 @@
     texturePatternBrush.source = img;
   }
 
-  $("drawing-mode-selector").onchange = function () {
-    if (this.value === "hline") {
-      canvas.freeDrawingBrush = vLinePatternBrush;
-    } else if (this.value === "vline") {
-      canvas.freeDrawingBrush = hLinePatternBrush;
-    } else if (this.value === "square") {
-      canvas.freeDrawingBrush = squarePatternBrush;
-    } else if (this.value === "diamond") {
-      canvas.freeDrawingBrush = diamondPatternBrush;
-    } else if (this.value === "texture") {
-      canvas.freeDrawingBrush = texturePatternBrush;
-    } else {
-      canvas.freeDrawingBrush = new fabric[this.value + "Brush"](canvas);
-    }
+  if (drawingModeSelector) {
+    drawingModeSelector.onchange = function () {
+      if (this.value === "hline") {
+        canvas.freeDrawingBrush = vLinePatternBrush;
+      } else if (this.value === "vline") {
+        canvas.freeDrawingBrush = hLinePatternBrush;
+      } else if (this.value === "square") {
+        canvas.freeDrawingBrush = squarePatternBrush;
+      } else if (this.value === "diamond") {
+        canvas.freeDrawingBrush = diamondPatternBrush;
+      } else if (this.value === "texture") {
+        canvas.freeDrawingBrush = texturePatternBrush;
+      } else {
+        canvas.freeDrawingBrush = new fabric[this.value + "Brush"](canvas);
+      }
 
-    if (canvas.freeDrawingBrush) {
+      if (canvas.freeDrawingBrush) {
+        var brush = canvas.freeDrawingBrush;
+        brush.color = drawingColorEl.value;
+        if (brush.getPatternSrc) {
+          brush.source = brush.getPatternSrc.call(brush);
+        }
+        brush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
+        brush.shadow = new fabric.Shadow({
+          blur: parseInt(drawingShadowWidth.value, 10) || 0,
+          offsetX: 0,
+          offsetY: 0,
+          affectStroke: true,
+          color: drawingShadowColorEl.value,
+        });
+      }
+    };
+  }
+
+  if(drawingColorEl) {
+    drawingColorEl.onchange = function () {
       var brush = canvas.freeDrawingBrush;
-      brush.color = drawingColorEl.value;
+      brush.color = this.value;
       if (brush.getPatternSrc) {
         brush.source = brush.getPatternSrc.call(brush);
       }
-      brush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
-      brush.shadow = new fabric.Shadow({
-        blur: parseInt(drawingShadowWidth.value, 10) || 0,
-        offsetX: 0,
-        offsetY: 0,
-        affectStroke: true,
-        color: drawingShadowColorEl.value,
-      });
-    }
-  };
+    };
+  }
 
-  drawingColorEl.onchange = function () {
-    var brush = canvas.freeDrawingBrush;
-    brush.color = this.value;
-    if (brush.getPatternSrc) {
-      brush.source = brush.getPatternSrc.call(brush);
-    }
-  };
-  drawingShadowColorEl.onchange = function () {
-    canvas.freeDrawingBrush.shadow.color = this.value;
-  };
-  drawingLineWidthEl.onchange = function () {
-    canvas.freeDrawingBrush.width = parseInt(this.value, 10) || 1;
-    this.previousSibling.innerHTML = this.value;
-  };
-  drawingShadowWidth.onchange = function () {
-    canvas.freeDrawingBrush.shadow.blur = parseInt(this.value, 10) || 0;
-    this.previousSibling.innerHTML = this.value;
-  };
-  drawingShadowOffset.onchange = function () {
-    canvas.freeDrawingBrush.shadow.offsetX = parseInt(this.value, 10) || 0;
-    canvas.freeDrawingBrush.shadow.offsetY = parseInt(this.value, 10) || 0;
-    this.previousSibling.innerHTML = this.value;
-  };
+  if(drawingShadowColorEl) {
+    drawingShadowColorEl.onchange = function () {
+      canvas.freeDrawingBrush.shadow.color = this.value;
+    };
+  }
+
+  if(drawingLineWidthEl) {
+    drawingLineWidthEl.onchange = function () {
+      canvas.freeDrawingBrush.width = parseInt(this.value, 10) || 1;
+      this.previousSibling.innerHTML = this.value;
+    };
+  }
+  
+  if(drawingShadowWidth) {
+    drawingShadowWidth.onchange = function () {
+      canvas.freeDrawingBrush.shadow.blur = parseInt(this.value, 10) || 0;
+      this.previousSibling.innerHTML = this.value;
+    };
+  }
+
+  if(drawingShadowOffset) {
+    drawingShadowOffset.onchange = function () {
+      canvas.freeDrawingBrush.shadow.offsetX = parseInt(this.value, 10) || 0;
+      canvas.freeDrawingBrush.shadow.offsetY = parseInt(this.value, 10) || 0;
+      this.previousSibling.innerHTML = this.value;
+    };
+  }
 
   if (canvas.freeDrawingBrush) {
     canvas.freeDrawingBrush.color = drawingColorEl.value;
